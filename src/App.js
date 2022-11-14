6c97ead1664535bcfc29e6231fa12314ea961fb8
Thiliban <thilip2017@gmail.com>
2022-11-14 14:10:57 +0530

    auto play updated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,20 @@ function App() {
     if (index < 0) {
       setIndex(lastIndex);
     }
-  },[index, people]);
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 3000);
+    return ()=>{
+      
+      clearInterval(slider);
+    }
+  }, [index]);
 
   return (
     <section className="section">
